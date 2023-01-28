bb1806c3857b35907f41951aa0b6403bee1eab6a
Mohd Hassan <mohd.hasan0777@gmail.com>
2023-01-28 14:44:15 +0530

    second commit

--- foodweb/settings.py
+++ foodweb/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path,os
 
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a@c#g3kj-j*@2a70b9ix@)33-#v5iqe6sq^owx!k!q00b#$wu%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
